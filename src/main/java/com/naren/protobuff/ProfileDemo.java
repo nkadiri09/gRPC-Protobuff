fbda45694ce13ea0b1095a46937da1f7eea484f2
“nkadiri09@gmail.com” <nkadiri09@gmail.com>
2024-02-07 13:48:23 -0500

    Serialize file

--- src/main/java/com/naren/protobuff/ProfileDemo.java
+++ src/main/java/com/naren/protobuff/ProfileDemo.java
@@ -2,12 +2,25 @@ package com.naren.protobuff;
 
 import com.naren.models.Profile;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class ProfileDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Profile person1 = Profile.newBuilder()
                 .setAge(22)
-                .setName("Narenda")
+                .setName("Narendra")
                 .build();
-        System.out.println("Person is:"+person1);
+        System.out.println("Person is: " + person1);
+
+        Path path = Paths.get("sam.txt");
+
+        Files.write(path, person1.toByteArray());
+
+        byte[] bytes = Files.readAllBytes(path);
+        Profile newPerson = Profile.parseFrom(bytes);
+        System.out.println("newPerson is: "+newPerson);
     }
 }
