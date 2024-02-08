3482c925ce26a3e70bfb320fa07cf68fee262ba2
“nkadiri09@gmail.com” <nkadiri09@gmail.com>
2024-02-07 21:37:13 -0500

    add role map in profile

--- src/main/java/com/naren/protobuff/ProfilePackageAddressDemo.java
+++ src/main/java/com/naren/protobuff/ProfilePackageAddressDemo.java
@@ -4,22 +4,24 @@ import com.naren.models.Address;
 import com.naren.models.Package;
 import com.naren.models.Profile;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ProfilePackageAddressDemo {
 
     public static void main(String[] args) {
 
         Package pac = Package.newBuilder().setName("TestPack").setType("Post").setWeight(10).build();
-        List<Package> packageList = new ArrayList<Package>();
-        packageList.add(pac);
+        Map<Integer, String> roleMap = new HashMap<>();
+        roleMap.put(2,"TestRol2");
         Address alp = Address.newBuilder().setStreet("623 Estuary trail").setCity("Alp").setZip(56002).build();
         Profile profile = Profile.newBuilder()
                 .setAge(22)
                 .setName("Narendra")
                 .setAddress(alp)
-                .addPackages(pac)
+                .addPackage(pac)
+                .putRole(1, "TestRole")
+                // .putAllRole(roleMap) can add multiple key value pairs
                 .build();
         System.out.println("profile is: "+profile);
 
